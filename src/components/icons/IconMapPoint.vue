afd559aa3147f8a1b0e5b5ff204a6d6d253db250
Justin Horner <mail@justinhorner.me>
2024-02-26 12:12:36 -0500

    change to use ionicons

--- src/components/icons/IconMapPoint.vue
+++ src/components/icons/IconMapPoint.vue
@@ -8,11 +8,15 @@ export default {
     height: {
       type: [Number, String],
       default: 18
+    },
+    color: {
+        type: String,
+        default: '#63E6BE'
     }
   }
 }
 </script>
 
 <template>
-    <svg xmlns="http://www.w3.org/2000/svg" :width="width" :height="height" viewBox="0 0 384 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#63E6BE" d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z"/></svg>
+    <svg xmlns="http://www.w3.org/2000/svg" :width="width" :height="height" :color="color" class="ionicon" viewBox="0 0 512 512"><path d="M256 48c-79.5 0-144 61.39-144 137 0 87 96 224.87 131.25 272.49a15.77 15.77 0 0025.5 0C304 409.89 400 272.07 400 185c0-75.61-64.5-137-144-137z" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32"/><circle cx="256" cy="192" r="48" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32"/></svg>
 </template>
