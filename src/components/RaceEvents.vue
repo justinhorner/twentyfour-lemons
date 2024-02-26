02e826a566328174b429d1416abe2c3793df7c01
Justin Horner <mail@justinhorner.me>
2024-02-26 12:16:13 -0500

    progress on syncing list and map points
    update to ensure initial event is not in the past
    updated UI for selected item
    update to scroll to selected item on map point selection

--- src/components/RaceEvents.vue
+++ src/components/RaceEvents.vue
@@ -11,6 +11,7 @@
 
     const data = reactive({ events: [], center: { longitude: 0.0, latitude: 0.0} });
     const mapCenter = ref([0, 0]);
+    const selectedEventName = ref('');
 
     /** 
      * TODOs:
@@ -26,8 +27,8 @@
      */
 
     onMounted(() => {
-        console.log('RaceEvents mounted');
-        console.log(`Found ${eventsJson?.length} events in json`);
+        //console.log('RaceEvents mounted');
+        //console.log(`Found ${eventsJson?.length} events in json`);
         const tempEvents = buildEventObjects();
         const sortedEvents = [...tempEvents].sort(e => e.startDate);
         data.events = sortedEvents;
@@ -43,10 +44,11 @@
 
         map.add(graphicsLayer);
 
-        const centerLong = data.events[0].location.coordinates.longitude;
-        const centerLat = data.events[0].location.coordinates.latitude;
+        const initialIndex = getInitialSelectedEventIndex();
+        const centerLong = data.events[initialIndex].location.coordinates.longitude;
+        const centerLat = data.events[initialIndex].location.coordinates.latitude;
 
-        mapCenter.value = [centerLong, centerLat];
+        processEventSelection(centerLong, centerLat, data.events[initialIndex].name);
 
         const view = new MapView({
             container: "viewDiv",
@@ -64,6 +66,23 @@
             }
         });
 
+        view.on('click', function(event) {
+            let screenPoint = {
+                x: event.x,
+                y: event.y
+            };
+
+            // find graphics at the location
+            view.hitTest(screenPoint)
+                .then((response) => {
+                    if (response.results.length) {
+                        let graphic = response.results.filter((result) => 
+                            result.graphic.layer === graphicsLayer)[0].graphic;
+                        handleMapPointClick(graphic.attributes.Name);
+                    }
+                })
+        });
+
         //build points for map graphics
         data.events.forEach((e) => {
             const point = buildMapPointGraphic(e);
@@ -78,29 +97,47 @@
         return selectedEvent;
     }
 
-
-    function processEventSelection(eventItem) {
-        const long = eventItem.location.coordinates.longitude;
-        const lat = eventItem.location.coordinates.latitude;
-        mapCenter.value = [long, lat];
+    function processEventSelection(longitude, latitude, eventName) {
+        // TODO: add re-zoom when this happens
+        mapCenter.value = [longitude, latitude];
+        selectedEventName.value = eventName;
     }
 
     function eventClicked(event) {
         const element = event.target;
-        console.log(element.innerText);
+        //console.log(element.innerText);
         const selectedEvent = getEventByName(element.innerText);
         
         if (selectedEvent) {
-            processEventSelection(selectedEvent);
+            processEventSelection(
+                selectedEvent.location.coordinates.longitude,
+                selectedEvent.location.coordinates.latitude,
+                selectedEvent.name);
         }
     }
 
     function eventIconClicked(eventName, event) {
+        event.preventDefault();
         console.log(`You clicked and icon. ${eventName}`);
         const selectedEvent = getEventByName(eventName);
         
         if (selectedEvent) {
-            processEventSelection(selectedEvent);
+            processEventSelection(
+                selectedEvent.location.coordinates.longitude,
+                selectedEvent.location.coordinates.latitude,
+                selectedEvent.name);
+        }
+    }
+
+    function handleMapPointClick(eventName) {
+        const selectedEvent = getEventByName(eventName);
+        if (selectedEvent) {
+            processEventSelection(
+                selectedEvent.location.coordinates.longitude,
+                selectedEvent.location.coordinates.latitude,
+                selectedEvent.name);
+
+            document.getElementById(selectedEvent.key).scrollIntoView();
         }
     }
 
@@ -109,18 +146,19 @@
      */
     function buildEventObjects() {
         let raceEvents = [];
-        eventsJson.forEach(event => {
+        eventsJson.forEach((eventItem, i) => {
             const re = /([A-Za-z]{3})\s(\d+)\s-\s([A-Za-z]{3})\s(\d+),\s(\d+)/g;
-            let dateInfoStr = event.dateInfo;
+            let dateInfoStr = eventItem.dateInfo;
             const matches = [...dateInfoStr.matchAll(re)][0];
             let startDate = new Date(`${matches[1]} ${matches[2]} ${matches[5]}`);
             let endDate = new Date(`${matches[3]} ${matches[4]} ${matches[5]}`);
-            let temp = JSON.parse(JSON.stringify(event));
+            let temp = JSON.parse(JSON.stringify(eventItem));
             temp.dates = {
                 startDate: startDate,
                 endDate: endDate
             };
-
+            // add a key for uniqueness & id
+            temp.key = `eventItem_${i}`;
 
             raceEvents.push(temp);
         })
@@ -160,25 +198,44 @@
 
         return pointGraphic;
     }
+
+    function getEventClass(eventItem) {
+        return eventItem.name === selectedEventName.value
+            ? 'eventItem eventItemSelected'
+            : 'eventItem';
+    }
+
+    function getInitialSelectedEventIndex() {
+        let index = 0;
+        for (let i = 0; i < data.events.length; i++) {
+            if (new Date(Date.now()) < data.events[i].dates.endDate) {
+                index = i;
+                break;
+            }
+        }
+        return index;
+    }
 </script>
 
 <template>
     <div id="mapContainer">
         <div id="eventsContainer">
             <div 
-                class="eventItem"
                 @click="eventClicked"
-                v-for="event in data.events">
-                <h5>{{ event.name }}</h5>
+                v-for="eventItem in data.events"
+                :key="eventItem.key"
+                :id="eventItem.key"
+                :class="getEventClass(eventItem)">
+                <h5>{{ eventItem.name }}</h5>
                 <div class="eventMetadata">
-                    <div>When: {{ event.dateInfo }}</div>
-                    <div>Where: {{ event.eventCourse }}</div>
-                    <div class="mapPointLink" @click="eventIconClicked(event.name, $event)">
+                    <div>When: {{ eventItem.dateInfo }}</div>
+                    <div>Where: {{ eventItem.eventCourse }}</div>
+                    <div class="mapPointLink" @click="eventIconClicked(eventItem.name, $event)">
                         <IconMapPoint width="32" height="32" />
                     </div>
                     <div class="externalLink">
                         <a 
-                            :href="event.url" 
+                            :href="eventItem.url" 
                             target="_blank"
                             alt="Link to race on 24 Hours of Lemon site">
                             <IconExternalLink width="32" height="32" />
@@ -218,11 +275,15 @@
 .eventItem {
     direction: ltr;
     padding: 10px 0px 10px 3%;
-    border: 1px solid lightgray;
+    border: 1px solid rgb(72, 72, 72);
     border-radius: 5px;
     margin: 0 5px 5px 0;
     margin-bottom: 10px;
     position: relative;
+}
+
+.eventItemSelected {
+    border: 2px solid var(--accent-color);
 }
 
 .eventMetadata {
